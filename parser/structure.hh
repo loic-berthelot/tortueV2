943f3b65c7a1e6482a9ebd38d0a32aa1872e9462
sbellier <sbelliertoulouzou@gmail.com>
2022-12-17 14:01:14 +0100

    modifs structures

--- parser/structure.hh
+++ parser/structure.hh
@@ -1,4 +1,23 @@
+#include <vector>
+#include <string>
 
-class Structure{
-    
-}
+class Noeud{
+    //continent l'instruction à lancer
+};
+
+
+/* 
+*   L'arbre contient l'instruction à executer
+*   On fait un parcour par largeur de l'arbre
+*   Si l'instruction est une instruction simple (pas une boucle ou un if) on ajoute un
+*   
+*/
+class Arbre{
+private:
+    std::vector<Arbre> fils;
+    std::string _instruction; //peut être pas un string, contient l'instruction à executer
+public:
+    Arbre(std::string _instruction);
+    void parcourir();
+    void ajouterFils(std::string _instruction);
+};
